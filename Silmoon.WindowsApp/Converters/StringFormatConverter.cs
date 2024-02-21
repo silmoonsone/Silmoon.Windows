b84ee459a2e87f8d2d9399e0ac4215e01f822f83
WEIBIN SONG <sone@silmoon.com>
2024-02-22 01:18:33 +0800

    增加一个用于WinUI3格式化绑定数据的转换器。

---
+++ Silmoon.WindowsApp/Converters/StringFormatConverter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silmoon.WindowsApp.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        // This converts the value object to the string to display.
+        // This will work with most simple types.
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return null;
+            // Retrieve the format string and use it to format the value.
+            string formatString = parameter as string;
+            if (!string.IsNullOrEmpty(formatString))
+            {
+                return string.Format(formatString, value);
+            }
+
+            // If the format string is null or empty, simply
+            // call ToString() on the value.
+            return value.ToString();
+        }
+
+        // No need to implement converting back on a one-way binding
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

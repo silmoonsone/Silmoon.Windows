521034a2dfcc3ab4d0ad0acca14aa1981318b3ce
WEIBIN SONG <sone@silmoon.com>
2024-03-24 22:32:06 +0800

    增加了WinForm相关帮助和扩展项目。

---
+++ Silmoon.Windows.Forms/FormExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace Silmoon.Windows.Forms
+{
+    public static class FormExtension
+    {
+        public static void SetCenterControl(this Form form, Control control, bool setXCenter = true, bool setYCenter = true)
+        {
+            // 计算窗体的中心点
+            int centerX = form.ClientSize.Width / 2;
+            int centerY = form.ClientSize.Height / 2;
+
+            // 根据Panel的尺寸更新它的位置，以使其居中
+            if (setXCenter)
+                control.Left = centerX - control.Width / 2;
+            if (setYCenter)
+                control.Top = centerY - control.Height / 2;
+        }
+    }
+}

9e12c8302cf3255b0bd216482936ad82fecae439
WEIBIN SONG <sone@silmoon.com>
2024-11-16 20:35:41 +0800

    内部更新整理。

--- Silmoon.Windows.Win32/Settings.cs
+++ Silmoon.Windows.Win32/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using Silmoon.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,39 +10,32 @@ namespace Silmoon.Windows.Win32
     {
         public static void SetAutoRun(string objectName, string path)
         {
-            RegistryKey hklm = Registry.LocalMachine;
-            try
+            using RegistryKey hklm = Registry.LocalMachine;
+            CSharpHelper.TryRun(() =>
             {
                 RegistryKey run = hklm.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", RegistryKeyPermissionCheck.ReadWriteSubTree);
                 run.SetValue(objectName, path, RegistryValueKind.String);
                 hklm.Close();
-            }
-            catch
-            { }
+            });
         }
         public static void DelAutoRun(string objectName)
         {
-            RegistryKey hklm = Registry.LocalMachine;
-            try
+            using RegistryKey hklm = Registry.LocalMachine;
+            CSharpHelper.TryRun(() =>
             {
                 RegistryKey run = hklm.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", RegistryKeyPermissionCheck.ReadWriteSubTree);
                 run.DeleteValue(objectName, false);
-                hklm.Close();
-            }
-            catch
-            { }
+            });
         }
         public static bool IsAutoRun(string objectName)
         {
-            RegistryKey hklm = Registry.LocalMachine;
-            RegistryKey run = hklm.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
-            string[] names = run.GetValueNames();
-            foreach (string name in names)
+            using RegistryKey hklm = Registry.LocalMachine;
+            var result = CSharpHelper.TryRun(() =>
             {
-                if (name == objectName)
-                    return true;
-            }
-            return false;
+                RegistryKey run = hklm.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+                return run.GetValueNames().Contains(objectName);
+            });
+            return result.State;
         }
     }
 }
